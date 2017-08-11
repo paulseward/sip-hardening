51197e86125f3f32cbc7457ae53ede3b10738d27
Paul Seward <paul.seward@bristol.ac.uk>
2017-08-11 14:17:27 +0100

    converted list to a more friendly bash loop, and added ip6tables support

--- iptables-sip.sh
+++ iptables-sip.sh
@@ -1,17 +1,27 @@
-iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "sipcli" --algo bm --to 65535 -j DROP
-iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "sipvicious" --algo bm --to 65535 -j DROP
-iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "sip-scan" --algo bm --to 65535 -j DROP
-iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "sipsak" --algo bm --to 65535 -j DROP
-iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "sundayaddr" --algo bm --to 65535 -j DROP
-iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "friendly-scanner" --algo bm --to 65535 -j DROP
-iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "iWar" --algo bm --to 65535 -j DROP
-iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "CSipSimple" --algo bm --to 65535 -j DROP
-iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "SIVuS" --algo bm --to 65535 -j DROP
-iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "Gulp" --algo bm --to 65535 -j DROP
-iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "sipv" --algo bm --to 65535 -j DROP
-iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "smap" --algo bm --to 65535 -j DROP
-iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "friendly-request" --algo bm --to 65535 -j DROP
-iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "VaxIPUserAgent" --algo bm --to 65535 -j DROP
-iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "VaxSIPUserAgent" --algo bm --to 65535 -j DROP
-iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "siparmyknife" --algo bm --to 65535 -j DROP
-iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "Test Agent" --algo bm --to 65535 -j DROP
+#!/bin/bash
+
+UAGENTS=(
+  "sipcli"
+  "sipvicious"
+  "sip-scan"
+  "sipsak"
+  "sundayaddr"
+  "friendly-scanner"
+  "iWar"
+  "CSipSimple"
+  "SIVuS"
+  "Gulp"
+  "sipv"
+  "smap"
+  "friendly-request"
+  "VaxIPUserAgent"
+  "VaxSIPUserAgent"
+  "siparmyknife"
+  "Test Agent"
+)
+
+for UA in "${UAGENTS[@]}"
+do
+  iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "${UA}" --algo bm --to 65535 -j DROP
+  ip6tables -A INPUT -p udp -m udp --dport 5060 -m string --string "${UA}" --algo bm --to 65535 -j DROP
+done
