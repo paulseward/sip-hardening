9ad1ab5c6e3f6f6224043f2ef8e0548097089c85
Paul Seward <paul.seward@bristol.ac.uk>
2017-08-11 15:30:36 +0100

    making matches insensitive, and specifically matching the User-Agent field.  Also added some UA's from http://support.polycom.com/content/dam/polycom-support/global/documentation/h.323-sip-botnet-bulletin-v1.4.pdf

--- iptables-sip.sh
+++ iptables-sip.sh
@@ -2,8 +2,15 @@
 
 UAGENTS=(
   "sipcli"
+  "PBX"
+  "voip"
+  "sipcli"
+  "pplsip"
   "sipvicious"
   "sip-scan"
+  "sipscan"
+  "xlite"
+  "MizuPhone"
   "sipsak"
   "sundayaddr"
   "friendly-scanner"
@@ -22,6 +29,6 @@ UAGENTS=(
 
 for UA in "${UAGENTS[@]}"
 do
-  iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "${UA}" --algo bm --to 65535 -j REJECT
-  ip6tables -A INPUT -p udp -m udp --dport 5060 -m string --string "${UA}" --algo bm --to 65535 -j REJECT
+  iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "User-Agent: ${UA}" --algo bm --icase --to 65535 -j REJECT
+  ip6tables -A INPUT -p udp -m udp --dport 5060 -m string --string "User-Agent: ${UA}" --algo bm --icase --to 65535 -j REJECT
 done
