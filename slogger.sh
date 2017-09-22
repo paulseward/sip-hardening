9058897a918db10e5521961982fe2925da322ed3
Paul Seward <paul.seward@bristol.ac.uk>
2017-09-22 15:19:10 +0100

    git ignore nohup.out

--- slogger.sh
+++ slogger.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # rough and ready SIP User-Agent logging, needs tshark installed
-tshark -l -f "port 5060 and src not 192.168.1.100" -Y '(sip.Method=="REGISTER") or (sip.Method=="INVITE") or (sip.Method=="OPTIONS")' -T fields -e ip.src -e ip.dst -e sip.Method -e sip.User-Agent -e sip.auth.uri -e sip.To -E header=y -E separator=, > /var/log/sip_ua.log 2>&1
+tshark -l -f "port 5060 and src not 192.168.1.100" -Y '(sip.Method=="REGISTER") or (sip.Method=="INVITE") or (sip.Method=="OPTIONS")' -T fields -e ip.src -e ip.dst -e sip.Method -e sip.User-Agent -e sip.auth.uri -e sip.To -E header=y -E separator=, > /tmp/sip_ua.log 2>&1
